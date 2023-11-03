f5a7edd22f39caaa180e92108f141ac02a74cf99
eyekavya <kavyasrivastava7830@gmail.com>
2023-11-03 17:44:20 +0530

    added background in homepage and modified character data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,15 @@ import React from "react";
 import HeroSection from "../HeroSection/HeroSection";
 import CharactersCard from "../CharactersCard/CharactersCard";
 import Footer from "../Footer";
+import { StyledHome } from "./StyledHome";
 
 function Home() {
   return (
-    <>
+    <StyledHome>
       <HeroSection />
       <CharactersCard />
       <Footer />
-    </>
+    </StyledHome>
   );
 }
 
---
+++ src/components/Home/StyledHome.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import theme from "./../../images/background.webp";
+
+export const StyledHome = styled.section`
+  background-image: linear-gradient(to bottom, #00000010 100%, #00000000),
+    url(${theme});
+`;
